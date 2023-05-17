80d9ae55c7feddf53a7946eaeac1bd816f4c9796
Alessio Lovato <alessio.lovato.1@studenti.unipd.it>
2023-05-17 18:42:35 +0200

    Switched to an example of reading given by Contiki. 'dht.c' and 'dht.h' file are no more required since it's possible to insert them from other folder of contiki (this is why Makefile changed).
    It is important to configure the port of DHT22 configuration of 'sensor_node.c' file based on data available in 'board.h' file for firefly-reva (board used)

--- projects/Final_project/sensor_node/sensor_node.c
+++ projects/Final_project/sensor_node/sensor_node.c
@@ -2,36 +2,41 @@
 #include <stdio.h>
 #include "sys/etimer.h"
 
-#define SECONDS_BETWEEN_READS 5 //Seconds between each read
+#define SECONDS_BETWEEN_READS 3 //Seconds between each read: >= 2
 
-#include "dht22.h"
+// DHT22 CONFIGURATION
+#define DHT22_CONF_PIN		I2C_SCL_PIN
+#define DHT22_CONF_PORT 	I2C_SCL_PORT
+#include "dev/dht22.h"
 
-PROCESS(example_process, "Example Process");
-AUTOSTART_PROCESSES(&example_process);
 
-PROCESS_THREAD(example_process, ev, data)
+PROCESS(dht22_process, "DHT22 process");
+AUTOSTART_PROCESSES(&dht22_process);
+
+PROCESS_THREAD(dht22_process, ev, data)
 {
   PROCESS_BEGIN();
 
-  static struct etimer timer;   
+  static struct etimer timer_sensor;
+  SENSORS_ACTIVATE(dht22); //activation of the sensor
+  int16_t temperature, humidity;
 
-  while (1) {
-    int16_t temperature;
-    int16_t humidity;
-    int8_t status = configure_DHT22(SENSORS_ACTIVE, 1);  //activation of the sensor
+  while(1) {
+	
+    etimer_set(&timer_sensor, CLOCK_SECOND * SECONDS_BETWEEN_READS);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_sensor));
 
-    if (status == DHT22_SUCCESS)
-    	status = dht22_read_all(&temperature, &humidity);
-
-    if (status == DHT22_SUCCESS) {
-    	printf("Temperature: %d°C, Humidity: %d%%\n", temperature/10, humidity/10); //data are shown as integer in the print but sent in complete form to the receiver
+    /* The standard sensor API may be used to read sensors individually, using
+     * the `dht22.value(DHT22_READ_TEMP)` and `dht22.value(DHT22_READ_HUM)`,
+     * however a single read operation (5ms) returns both values, so by using
+     * the function below we save one extra operation
+     */
+    if(dht22_read_all(&temperature, &humidity) != DHT22_ERROR) {
+      printf("Temperature %02d.%02d ºC, ", temperature / 10, temperature % 10);
+      printf("Humidity %02d.%02d RH\n", humidity / 10, humidity % 10);
     } else {
-    	printf("Failed to read data from DHT22 sensor. Error code: %d\n", status);
+      printf("Failed to read data from DHT22 sensor.");
     }
-
-    // Delay before the next reading
-    etimer_set(&timer, CLOCK_SECOND * SECONDS_BETWEEN_READS);
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
   }
 
   PROCESS_END();
