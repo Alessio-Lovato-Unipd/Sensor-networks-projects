9ee6e85f75e248ce48ec82b183119f4bcebb5e9f
Alessio Lovato <alessio.lovato.1@studenti.unipd.it>
2023-05-24 11:28:58 +0200

    Added some constants and a header for TSCH

--- projects/Final_project/network_configuration.h
+++ projects/Final_project/network_configuration.h
@@ -2,6 +2,8 @@
 #include "net/netstack.h"
 #include "net/ipv6/simple-udp.h"
 #include "sys/log.h"
+
+#include "net/mac/tsch/tsch.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
@@ -10,20 +12,15 @@
 #define UDP_CLIENT_PORT	8765
 #define UDP_SERVER_PORT	5678
 
-#define SECONDS_BETWEEN_TRANSMISSION 3
-#define SEND_INTERVAL		  (SECONDS_BETWEEN_TRANSMISSION * CLOCK_SECOND)
+// Time of connection to network after MAX_NUM_OF_MISSING
+#define RECONNECTION_SECONDS 10
 
+// Max number of packets not delivered
+#define MAX_NUM_OF_MISSING 5
 
+// Time between reconnection after MAX_NUM_OF_MISSING
+#define SECONDS_BETWEEN_RECONNECTION 20
 
-// UDP receiver callback
-
-void udp_sensor_callback(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen);
 
 // PACKETS EXCHANGED 
 struct packet {
